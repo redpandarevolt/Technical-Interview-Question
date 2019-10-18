be8051882dd4d64c74361495f383f0f6cb50eec7
DragonGirl <redpandarevolt@gmail.com>
2019-10-18 14:48:31 -0700

    Update ContainsDuplicates.cpp
    Added Solution 2 using Sort

--- ContainsDuplicates.cpp
+++ ContainsDuplicates.cpp
@@ -17,9 +17,32 @@ public:
             {
                 if (nums[i] == nums[j])
                 {
-                    cout << "true";
+                    cout << "solution 1 holds: true" << endl;
                     return true;
                 }
+            }
+        }
+        return false;
+    }
+};
+
+class Solution2 {
+public:
+    bool containsDuplicate2(vector<int>& nums) {
+
+        if(nums.empty() || (nums.size() == 1))
+        {
+            return false;
+        }
+
+        sort(nums.begin(), nums.end());
+
+        for(int i= 0; i < nums.size() - 1 ; i++)
+        {
+            if (nums[i] == nums[i+ 1])
+            {
+                cout << "solution 2 holds: true" << endl;
+                return true;
             }
         }
         return false;
@@ -30,7 +53,10 @@ public:
 int main()
 {
     Solution result;
+    Solution2 result2;
     vector<int> test1 {1,2,3,4,2,6,4,9};
     result.containsDuplicate(test1);
+    result2.containsDuplicate2(test1);
+
 
 };
